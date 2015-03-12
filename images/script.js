1f2e92fb080d1f6a1c2cf2cd031c92fa9abcebea
clong <clong@wdtinc.com>
2015-03-11 22:40:24 -0500

    updating files

---
+++ images/script.js
@@ -0,0 +1,14 @@
+function showContent() {
+	document.getElementById("hiddenContent").style.display="block";
+	if(document.getElementById("hiddenContact").style.display="block") {
+			document.getElementById("hiddenContact").style.display="none";
+	}
+}
+function showContact() {
+	document.getElementById("hiddenContact").style.display="block";
+	if (document.getElementById("hiddenContent").style.display="block") {
+			document.getElementById("hiddenContent").style.display="none";
+	}
+}
+function hideContent() {document.getElementById("hiddenContent").style.display="none";}
+function hideContact() {document.getElementById("hiddenContact").style.display="none";}
